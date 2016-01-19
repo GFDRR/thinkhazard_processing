0142a734374f84ad39817f71fcf5e99205741f8e
François Van Der Biest <francois.vanderbiest@camptocamp.com>
2016-01-19 11:30:13 +0100

    GPL v3 license

--- thinkhazard_processing/tests/__init__.py
+++ thinkhazard_processing/tests/__init__.py
@@ -1,3 +1,27 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2015- by the GFDRR / World Bank
+#
+# This file is part of ThinkHazard.
+#
+# ThinkHazard is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option)
+# any later version.
+#
+# ThinkHazard is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# ThinkHazard.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Author: Pierre Giraud <pierre.giraud@camptocamp.com>
+# Author: Arnaud Morvan <arnaud.morvan@camptocamp.com>
+# Author: François Van Der Biest <francois.vanderbiest@camptocamp.com>
+#
+
 import yaml
 from sqlalchemy import engine_from_config
 from .. import settings
